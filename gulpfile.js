541072ee95a428e6cc8b831dbb34e3e6c2ec1a5f
Denys Gudakov <denysgud@gmail.com>
2016-10-08 21:41:40 +0300

    Partially implemented mobile version of header section

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 
 gulp.task('less', function(){
-  return gulp.src('assets/less/main.less')
+  return gulp.src('client/assets/less/main.less')
     .pipe(less()) // Using gulp-sass
-    .pipe(gulp.dest('assets/css'))
+    .pipe(gulp.dest('client/assets/css'))
 });
 
 gulp.task('watch', function(){
-  gulp.watch('assets/less/main.less', ['less']); 
+  gulp.watch('client/assets/less/main.less', ['less']); 
 });
